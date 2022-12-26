5539cd1631bb3878529823a596752618b711e7ec
HUNG WEI-CHING <hwc0320@gmail.com>
2022-12-26 16:21:17 +0800

    Initial design of the game

---
+++ app.js
@@ -0,0 +1,20 @@
+let floor = 1;
+let ceil = 100;
+let answer = Math.ceil(100 * Math.random());
+window.alert("從1~100中猜一個數字吧!\n猜到就算你贏了!");
+let guess;
+do {
+  console.log(answer);
+  guess = window.prompt("請猜一個介於" + floor + "~" + ceil + "的數字:");
+  if (guess < floor || guess > ceil) {
+    window.alert("請輸入範圍內的數字");
+    break;
+  } else if (guess < answer) {
+    floor = guess;
+  } else if (guess > answer) {
+    ceil = guess;
+  } else {
+    break;
+  }
+} while (guess !== answer);
+window.alert("恭喜猜對了，終極密碼是" + answer + "!");
